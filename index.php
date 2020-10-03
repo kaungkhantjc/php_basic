fd7267ea61f96da6b0d4db3527c447f3024015a2
Kaung Khant Kyaw <58139120+kaungkhantjc@users.noreply.github.com>
2020-10-03 14:06:55 +0630

    Added Drawer Menu
    Added Drawer Menu

--- index.php
+++ index.php
@@ -27,11 +27,54 @@ if (isset($_POST['save_info'])) saveInfo();
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="css/main.css">
+
+    <style>
+        .menu {
+            cursor: pointer;
+            border-radius: 50%;
+            width: 45px;
+            height: 45px;
+            margin-top: 3px;
+            position: absolute;
+        }
+
+        .slideOutLeft {
+            animation-name: slideOut;
+            animation-duration: .5s;
+        }
+
+        @keyframes slideOut {
+            0% {
+                transform: translateX(0px);
+            }
+            100% {
+                transform: translateX(-200px);
+            }
+        }
+
+
+    </style>
 </head>
 
 <main id="main">
+
+    <div class="w3-sidebar w3-bar-block w3-animate-left w3-card-4 w3-hide" id="drawer">
+        <div style="background-image: url('https://i.pinimg.com/originals/c2/bd/3a/c2bd3ae483f9617e6f71bc2a74b60b5a.jpg'); background-size: cover;"
+             class="w3-green w3-padding-large">
+            <h5>Title</h5>
+            <p>abcd@gmail.com</p>
+        </div>
+        <a onclick="w3_close()" class="w3-bar-item w3-button">Home</a>
+        <a onclick="w3_close()" class="w3-bar-item w3-button">About</a>
+        <a onclick="w3_close()" class="w3-bar-item w3-button">Privacy Policy</a>
+    </div>
+
     <header id="header" class="w3-card-4 w3-green">
-        <h3>Admin Panel</h3>
+        <div class="menu w3-display-container" onclick="w3_open()">
+            <div id="menuIcon" class="w3-display-middle w3-xlarge w3-animate-opacity">☰</div>
+        </div>
+        <h3 style="margin-left: 55px">Admin Panel</h3>
+
         <a class="w3-button w3-hover-opacity" href="?logout=1">
             <img style="padding: 4px" class="w3-display-right w3-margin-right w3-circle w3-pale-green"
                  src="https://img.icons8.com/android/24/000000/logout-rounded.png"
@@ -39,7 +82,7 @@ if (isset($_POST['save_info'])) saveInfo();
         </a>
     </header>
 
-    <div id="contents" class="content">
+    <div onclick="w3_close()" id="contents" class="content">
         <div class="w3-panel w3-container">
 
             <?php $info_row = readInfo() ?>
@@ -48,13 +91,13 @@ if (isset($_POST['save_info'])) saveInfo();
             <form class="w3-panel w3-container" method="post">
                 <label for="edt_title">
                     <input id="edt_title" class="w3-input" type="text" name="title" placeholder="Title"
-                           value="<?php if($info_row) echo $info_row['title'] ?>"/>
+                           value="<?php if ($info_row) echo $info_row['title'] ?>"/>
                 </label>
 
                 <label for="edt_message">
                     <input id="edt_message" class="w3-input w3-section" type="text" name="message"
                            placeholder="Message"
-                           value="<?php if($info_row) echo $info_row['message'] ?>"/>
+                           value="<?php if ($info_row) echo $info_row['message'] ?>"/>
                 </label>
 
                 <label for="show_info_switch">
@@ -92,5 +135,39 @@ if (isset($_POST['save_info'])) saveInfo();
 
         </div>
     </div>
+
+
 </main>
+
+<script src="https://hammerjs.github.io/dist/hammer.js"></script>
+<script>
+
+    let drawer = document.getElementById("drawer");
+    let menuIcon = document.getElementById('menuIcon');
+
+    function w3_open() {
+        if (drawer.classList.contains("w3-hide")){
+            drawer.classList.remove("w3-hide");
+            menuIcon.innerText = "⬅";
+        } else w3_close();
+    }
+
+    function w3_close() {
+        menuIcon.innerText = "☰";
+        drawer.classList.remove("w3-animate-left");
+        drawer.classList.add("slideOutLeft");
+        setTimeout(function () {
+                drawer.classList.add("w3-hide");
+                drawer.classList.add("w3-animate-left");
+                drawer.classList.remove("slideOutLeft");
+            }, 400);
+    }
+
+    let hammer = new Hammer(drawer);
+    hammer.on("panleft", function () {
+        w3_close();
+    });
+
+
+</script>
 </html>
